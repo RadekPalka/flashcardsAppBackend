6eb55ad34bb3978723faa179d7bfd3cb20783f87
Radosław Pałka <radekpalka@gmail.com>
2025-04-23 19:01:02 +0200

    feat: create User data

--- src/main/java/pl/radekpalka/flashcardsApp/model/User.java
+++ src/main/java/pl/radekpalka/flashcardsApp/model/User.java
@@ -1,4 +1,41 @@
 package pl.radekpalka.flashcardsApp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(){}
+
+    public User(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
